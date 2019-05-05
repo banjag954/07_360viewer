17f517126f4eddbb421c6df03ecb0cd9a1b64d24
ColumbusK <banjag954@naver.com>
2019-05-05 19:00:44 +0900

    responsive_work_finish_01

--- js/360viewersite.js
+++ js/360viewersite.js
@@ -1,4 +1,8 @@
-loading = ()=> {
+
+// 기능부
+
+// page_transition animation
+animationLoading = ()=> {
 
     let loader = document.getElementById('loading'),
     loadContents = document.getElementsByClassName('loading_content'),
@@ -36,45 +40,57 @@ loading = ()=> {
 
     // Appear mainPage
     appearMain = () => {
-        tlAppear = new TimelineLite({onComplete: start});
+        tlAppear = new TimelineLite({});
         // tlAppear.play();
         tlAppear
             .set(pageStart, {display:'block',visibility:'visible'})
             .to(pageStart,null,{autoAlpha: 1});
     }
-
-
-    start = () => {}
 }
 
-loading();
 
+// container_03 card animation
+animationCard = ()=> {
 
-var w = $(window).width();
-var h = $(window).height();
+  var w = $(window).width();
+  var h = $(window).height();
 
-$('.img_03').on('mousemove', function(e) {
-  var offsetX = 0.5 - e.pageX / w;
-  var offsetY = 0.5 - e.pageY / h; 
-  var dy = e.pageY - h / 2; 
-  var dx = e.pageX - w / 2;
-  var theta = Math.atan2(dy, dx); 
-  var angle = theta * 180 / Math.PI - 90;
-  var offsetPoster = $(this).data('offset');
-  var transformPoster = 'translateY(' + -offsetX * offsetPoster + 'px) rotateX(' + (-offsetY * offsetPoster) + 'deg) rotateY(' + (offsetX * (offsetPoster * 3)) + 'deg) scale(1.01)'; // poster transform
-  if (angle < 0) {
-    angle = angle + 360;
-  }
-  $(this).css({
-    'transform': transformPoster,
-    'box-shadow': '0 45px 60px rgba(14,21,47,0.2), 0 16px 40px rgba(14,21,47,0.2)',
+  $('.img_03').on('mousemove', function(e) {
+    var offsetX = 0.5 - e.pageX / w;
+    var offsetY = 0.5 - e.pageY / h; 
+    var dy = e.pageY - h / 2; 
+    var dx = e.pageX - w / 2;
+    var theta = Math.atan2(dy, dx); 
+    var angle = theta * 180 / Math.PI - 90;
+    var offsetPoster = $(this).data('offset');
+    var transformPoster = 'translateY(' + -offsetX * offsetPoster + 'px) rotateX(' + (-offsetY * offsetPoster) + 'deg) rotateY(' + (offsetX * (offsetPoster * 3)) + 'deg) scale(1.01)'; // poster transform
+    if (angle < 0) {
+      angle = angle + 360;
+    }
+    $(this).css({
+      'transform': transformPoster,
+      'box-shadow': '0 45px 60px rgba(14,21,47,0.2), 0 16px 40px rgba(14,21,47,0.2)',
+    });
   });
-});
 
-$('.img_03').on('mouseout', function(){
-  $(this).css({
-    'transform': 'translate(0px)',
-    'box-shadow': '0 8px 30px rgba(14,21,47,0.4)'
+  $('.img_03').on('mouseout', function(){
+    $(this).css({
+      'transform': 'translate(0px)',
+      'box-shadow': '0 8px 30px rgba(14,21,47,0.4)'
+    });
   });
-});
+
+}
+
+// 작동부
+
+if (matchMedia("screen and (min-width: 1025px)").matches) {
+
+  animationCard(),
+  animationLoading();
+  
+} else {
+
+  animationLoading();
+}
 
